c07ae5b4703e5bfe85f20b4bb09661d9d98145cb
Justin Nichols <51006450+jnichols93@users.noreply.github.com>
2020-12-03 17:23:38 -0600

    backend setup

---
+++ models/Post.js
@@ -0,0 +1,28 @@
+const { model , Schema } = require('mongoose');
+
+const PostSchema = new Schema({
+    username: String,
+    body: String,
+    createdAt: String,
+    comments: [
+        {
+         body: String,
+         username: String,
+         createdAt: String,
+    }
+    ],
+    likes:[
+        {
+        username: String, 
+        createdAt: String
+
+    }
+],
+user: {
+    type: Schema.Types.ObjectId,
+    ref: 'users'
+}
+
+});
+
+module.exports = model('Post', PostSchema)
